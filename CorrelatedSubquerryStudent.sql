8fe662da9006a1dce6c888d5803d93b46dfbd255
tanveermarne20 <>
2024-02-19 10:10:41 +0530

    student table updated

--- CorrelatedSubquerryStudent.sql
+++ CorrelatedSubquerryStudent.sql
@@ -8,6 +8,7 @@ WHERE ROWID !=(SELECT MAX(ROWID)
                 FROM STUDENT S2
                 WHERE S2.SID=S1.SID);
                 
+                
 
 -- A101 1	Anil	M	65
 
@@ -15,6 +16,19 @@ DELETE
 FROM STUDENT S1
 WHERE A101 !=(A107);   --here condition becomes true ,than compiler goes to delete
                            
-                           
+                
+                
+                
+--2,'Tanvir','M'	,45 
+DELETE 
+FROM STUDENT S1
+WHERE A102 !=(A106); -----here condition becomes true ,than compiler goes to delete
+
+                 
+ 
+ 
+        
+CREATE USER MERGEOPERATION IDENTIFIED BY root;
+GRANT ALL PRIVILEGES TO MERGEOPERATION;
 
                            
